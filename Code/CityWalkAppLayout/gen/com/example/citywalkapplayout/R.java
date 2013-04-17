36ccb7fb9c51bb9a09a3cea4f10e07b78ec5f908
Beechwood <hemmingsen90@gmail.com>
2013-04-17 14:09:40 +0200

    code update

--- Code/CityWalkAppLayout/gen/com/example/citywalkapplayout/R.java
+++ Code/CityWalkAppLayout/gen/com/example/citywalkapplayout/R.java
@@ -11,18 +11,40 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int checkeredflag=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int lakes=0x7f020002;
+        public static final int mermaid=0x7f020003;
+        public static final int stopwatch=0x7f020004;
     }
     public static final class id {
+        public static final int FROM_CELL=0x7f070007;
+        public static final int ImageView01=0x7f07000d;
+        public static final int ImageView02=0x7f07000b;
+        public static final int ImageView04=0x7f070013;
+        public static final int ImageView05=0x7f070011;
+        public static final int TO_CELL=0x7f070008;
+        public static final int TRAIN_CELL=0x7f070006;
+        public static final int dist1=0x7f07000c;
+        public static final int dist2=0x7f070012;
         public static final int horizontalScrollView1=0x7f070000;
-        public static final int menu_settings=0x7f070005;
+        public static final int icon1=0x7f070009;
+        public static final int icon2=0x7f07000f;
+        public static final int list=0x7f070005;
+        public static final int menu_settings=0x7f070015;
         public static final int textView1=0x7f070001;
         public static final int textView2=0x7f070002;
         public static final int textView3=0x7f070003;
         public static final int textView4=0x7f070004;
+        public static final int time1=0x7f07000e;
+        public static final int time2=0x7f070014;
+        public static final int title1=0x7f07000a;
+        public static final int title2=0x7f070010;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int copylist_2line=0x7f030001;
+        public static final int list_2line=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
