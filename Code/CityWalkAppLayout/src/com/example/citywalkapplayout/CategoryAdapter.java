69212f3bb4f8f691a70bae7a6be115aafcf3b5bf
Anders Michael Nielsen <gedenhesten@gmail.com>
2013-04-24 14:42:29 +0200

    Added browse category

---
+++ Code/CityWalkAppLayout/src/com/example/citywalkapplayout/CategoryAdapter.java
@@ -0,0 +1,40 @@
+package com.example.citywalkapplayout;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CategoryAdapter extends ArrayAdapter<String> {
+	private final Context context;
+	private final String[] values;
+
+	public CategoryAdapter(Context context, String[] values) {
+		super(context, R.layout.activity_category, values);
+		this.context = context;
+		this.values = values;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context
+			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+ 
+		View rowView = inflater.inflate(R.layout.activity_category, parent, false);
+		TextView textView = (TextView) rowView.findViewById(R.id.label);
+		ImageView imageView = (ImageView) rowView.findViewById(R.id.arrow);
+		textView.setText(values[position]);
+ 
+		// Change icon based on name
+		String s = values[position];
+ 
+		System.out.println(s);
+
+		imageView.setImageResource(R.drawable.right_arrow);
+ 
+		return rowView;
+	}
+}
